d72a5a22e0874ff03cc68e7ba9733fd01c5fe688
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-06-05 12:08:41 -0400

    Creates file name after params

--- hadoop-util/src/main/java/ca/uwaterloo/hadoop/util/DumpSeqFilesFolder.java
+++ hadoop-util/src/main/java/ca/uwaterloo/hadoop/util/DumpSeqFilesFolder.java
@@ -3,17 +3,23 @@ package ca.uwaterloo.hadoop.util;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.io.Writable;
 import org.apache.mahout.common.Pair;
+import org.apache.mahout.common.Parameters;
 import org.apache.mahout.freqtermsets.convertors.string.TopKStringPatterns;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class DumpSeqFilesFolder {
 	private static Logger L = LoggerFactory.getLogger("DumpSeqFilesFolder");
@@ -27,23 +33,55 @@ public class DumpSeqFilesFolder {
 //		int pHeapSize = 5000;
 
 		String seqPath = args[0];
-
-		PrintStream out = new PrintStream(new FileOutputStream(
-						 seqPath + File.separator + "assoc.csv"), 
-						true, "UTF-8");
-		seqPath += File.separator + "frequentpatterns";
-		
+    seqPath += File.separator + "frequentpatterns";
+    
 
 
 		FileSystem fs = FileSystem.get(new Configuration());
-		Path file = new Path(seqPath);
-		if (!fs.exists(file)) {
-			System.err.println("Error: " + file + " does not exist!");
+		Path inPath = new Path(seqPath);
+		if (!fs.exists(inPath)) {
+			System.err.println("Error: " + inPath + " does not exist!");
 			System.exit(-1);
 		}
 
+		
+		Path confPath = new Path(inPath, "_logs/history");
+		FileStatus[] confFile = fs.listStatus(confPath,new PathFilter() {
+      
+      public boolean accept(Path p) {
+        return p.getName().endsWith("_conf.xml");
+      }
+    });
+    Configuration conf = new Configuration();
+    
+    conf.addResource(confFile[0].getPath());
+    
+    Map<String, String> params = Parameters.parseParams(conf.get("pfp.parameters"));
+    params.remove("input");
+    params.remove("output");
+    params.remove("gfisIn");
+    params.remove("countIn");
+    params.remove("encoding");
+    
+    
+    StringBuilder outFilename = new StringBuilder("assoc");
+    
+    String[] keys = params.keySet().toArray(new String[0]);
+    Arrays.sort(keys);
+    
+    for(String key: keys) {
+      outFilename.append('_').append(key).append(params.get(key));
+    }
+    
+    SimpleDateFormat dateFmt = new SimpleDateFormat("MMddHHmm");
+    outFilename.append('_').append(dateFmt.format(new Date()));
+    
+    PrintStream out = new PrintStream(new FileOutputStream(
+          args[0] + File.separator + outFilename.toString() + ".csv"), 
+            true, "UTF-8");
+     
 		CorpusReader<Writable, TopKStringPatterns> stream = new CorpusReader<Writable, TopKStringPatterns>(
-				file, fs, "part.*");
+				inPath, fs, "part.*");
 //		HashMap<String, TopKStringPatterns> merged = new HashMap<String, TopKStringPatterns>();
 		try {
 			
