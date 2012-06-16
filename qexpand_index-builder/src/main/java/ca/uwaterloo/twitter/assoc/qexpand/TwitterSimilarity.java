45b875d7b6200f835d22d33f969f3eab7d323688
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-06-16 08:38:54 -0400

    Forgotten file.. nothing fancy in it.. just ignores tf

---
+++ qexpand_index-builder/src/main/java/ca/uwaterloo/twitter/assoc/qexpand/TwitterSimilarity.java
@@ -0,0 +1,17 @@
+package ca.uwaterloo.twitter.assoc.qexpand;
+
+import org.apache.lucene.search.DefaultSimilarity;
+
+public class TwitterSimilarity extends DefaultSimilarity {
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -4284745532074292161L;
+
+  @Override
+  public float tf(float freq) {
+    return freq>0?1.0f:0.0f;
+  }
+  
+}
