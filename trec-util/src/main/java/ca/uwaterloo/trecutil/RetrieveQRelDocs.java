fe31ff7a96588ec0d0e1341bdf15ecdb364dda58
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-07-05 13:02:18 -0400

    Retrieval of QRel Actual Tweets

---
+++ trec-util/src/main/java/ca/uwaterloo/trecutil/RetrieveQRelDocs.java
@@ -0,0 +1,54 @@
+package ca.uwaterloo.trecutil;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.channels.Channels;
+import java.util.LinkedHashMap;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.MMapDirectory;
+
+public class RetrieveQRelDocs {
+  
+  /**
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    QRelUtil qRelUtil = new QRelUtil(new File(args[0]));
+    Directory ixDir = MMapDirectory.open(new File(args[1]));
+    IndexReader ixReader = IndexReader.open(ixDir);
+    IndexSearcher ixSearcher = new IndexSearcher(ixReader);
+    Writer wr = Channels.newWriter(FileUtils.openOutputStream(new File(args[2])).getChannel(),
+        "UTF-8");
+    try {
+      for (String qid : qRelUtil.qRel.keySet()) {
+        LinkedHashMap<String, Float> qRel = qRelUtil.qRel.get(qid);
+        for (String docId : qRel.keySet()) {
+          TermQuery docQuery = new TermQuery(new Term("id", docId));
+          TopDocs rs = ixSearcher.search(docQuery, 100);
+          
+          if (rs.totalHits < 1) {
+            continue; // might be a deleted tweet
+          } else if (rs.totalHits > 1) {
+            throw new IllegalStateException();
+          }
+          String tweet = ixReader.document(rs.scoreDocs[0].doc).get("text");
+          wr.append(qid + "\t" + docId + "\t" + qRel.get(docId) + "\t" + tweet + "\n");
+        }
+      }
+    } finally {
+      wr.flush();
+      wr.close();
+      ixReader.close();
+    }
+  }
+  
+}
