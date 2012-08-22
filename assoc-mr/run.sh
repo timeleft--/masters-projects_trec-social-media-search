f97260ca544a8a80638f38296fd1448a34a72e36
Younos Aboulnaga <yaboulna@uwaterloo.ca>
2012-08-22 16:02:40 -0400

    Run scripts

--- assoc-mr/run.sh
+++ assoc-mr/run.sh
@@ -1,4 +1,4 @@
-/u2/yaboulnaga/Programs/hadoop-1.0.3/bin/hadoop jar target/assoc-mr-0.0.2-SNAPSHOT-job.jar org.apache.mahout.freqtermsets.FPGrowthDriver --input file:///u2/yaboulnaga/Shared/datasets/twitter-trec2011/csv_hour-5min/ --output file:///u2/yaboulnaga/Shared/datasets/twitter-trec2011/assoc-mr_mi_17days/ --countIn file:///u2/yaboulnaga/Shared/datasets/twitter-trec2011/assoc-mr_supp_17days/ --gfisIn file:///u2/yaboulnaga/Shared/datasets/twitter-trec2011/assoc-mr_supp_17days/ -mi -mf 3 --minSupport 2 -pct 99 -g 10240 -j 1
+/u2/yaboulnaga/Programs/hadoop-1.0.3/bin/hadoop jar target/assoc-mr-0.0.2-SNAPSHOT-job.jar org.apache.mahout.freqtermsets.FPGrowthDriver --input file:///u2/yaboulnaga/data/twitter-trec2011/rawtweets_csv_hour-5min/ --output file:///u2/yaboulnaga/data/twitter-trec2011/assoc-mr_sliding_1hr-5min/ -mf 3 --minSupport 2 -pct 99 -g 256 -j 1 -ws 3600000 -ss 300000
 
 # file:///u2/yaboulnaga/Shared/datasets/twitter-trec2011/csv_hour-5min/
 # -st 1295740800000
